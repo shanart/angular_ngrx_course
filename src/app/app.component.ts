16390797f761f68e106435e2439951192f3c32b8
shanart <shanin.x@gmail.com>
2023-02-13 23:22:02 +0200

    is logged in selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from "@ngrx/store";
 import { Observable } from "rxjs";
-import { map } from 'rxjs/operators';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { AppState } from './reducers';
+import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
 
 @Component({
     selector: 'app-root',
@@ -12,13 +13,14 @@ import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Rout
 export class AppComponent implements OnInit {
 
     loading = true;
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private store: Store<AppState>) {
 
     }
 
     ngOnInit() {
-
         this.router.events.subscribe(event => {
             switch (true) {
                 case event instanceof NavigationStart: {
@@ -37,7 +39,8 @@ export class AppComponent implements OnInit {
                 }
             }
         });
-
+        this.isLoggedIn$ = this.store.pipe(select(isLoggedIn));
+        this.isLoggedOut$ = this.store.pipe(select(isLoggedOut));
     }
 
     logout() {
