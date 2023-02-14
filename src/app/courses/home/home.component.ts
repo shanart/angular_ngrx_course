29ce0cc156327e18e86b4d8b8128a26b5472efd4
shanart <shanin.x@gmail.com>
2023-02-14 02:17:39 +0200

    sortComparer for adapter for sorting courses

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -24,7 +24,6 @@ export class HomeComponent implements OnInit {
     constructor(
         private dialog: MatDialog,
         private store: Store<AppState>) {
-
     }
 
     ngOnInit() {
--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,10 +1,13 @@
-import { Course } from "../model/course";
+import { compareCourses, Course } from "../model/course";
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CoursesActions } from "../action-types";
 
 export interface CoursesState extends EntityState<Course> {}
-export const adapter = createEntityAdapter<Course>();
+export const adapter = createEntityAdapter<Course>({
+    sortComparer: compareCourses
+    // selectId: course => course.id  // here you can set what field is the unique identifier for entity
+});
 export const initialCoursesState = adapter.getInitialState();
 
 export const coursesReducer = createReducer(
