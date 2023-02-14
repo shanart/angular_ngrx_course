ef199e352cda481174fed89b0dfb511bf28432d9
shanart <shanin.x@gmail.com>
2023-02-14 21:14:24 +0200

    implement services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, isDevMode } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HttpClientModule } from '@angular/common/http';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HttpClientModule} from '@angular/common/http';
 
-import { RouterModule, Routes } from '@angular/router';
-import { AuthModule } from './auth/auth.module';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthModule} from './auth/auth.module';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {RouterState, StoreRouterConnectingModule} from '@ngrx/router-store';
+
+import {EffectsModule} from '@ngrx/effects';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { metaReducers, reducers } from './reducers';
-import { AuthGuard } from './auth/auth.guard';
-import { EffectsModule } from '@ngrx/effects';
-import { RouterState, StoreRouterConnectingModule } from '@ngrx/router-store';
+import {metaReducers, reducers} from './reducers';
+import {AuthGuard} from './auth/auth.guard';
+import { EntityDataModule, EntityMetadata } from '@ngrx/data';
 
 
 const routes: Routes = [
@@ -51,20 +54,22 @@ const routes: Routes = [
         MatListModule,
         MatToolbarModule,
         AuthModule.forRoot(),
-        StoreModule.forRoot(
-            reducers, 
-            {
-                metaReducers, 
-                runtimeChecks: {
-                    strictActionImmutability: true,
-                    strictActionSerializability: true,
-                    strictStateSerializability: true
-                }
+        StoreModule.forRoot(reducers, {
+            metaReducers,
+            runtimeChecks : {
+                strictStateImmutability: true,
+                strictActionImmutability: true,
+                strictActionSerializability: true,
+                strictStateSerializability:true
             }
-        ),
-        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+        }),
+        StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
         EffectsModule.forRoot([]),
-        StoreRouterConnectingModule.forRoot({stateKey: "router", routerState: RouterState.Minimal })
+        EntityDataModule.forRoot({}),
+        StoreRouterConnectingModule.forRoot({
+            stateKey: 'router',
+            routerState: RouterState.Minimal
+        })
     ],
     bootstrap: [AppComponent]
 })
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -21,20 +21,34 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
+import { EntityDataService, EntityDefinitionService, EntityMetadataMap } from '@ngrx/data';
+import { CoursesDataService } from './services/courses-data.service';
+import { CourseEntityService } from './services/course-entity.service';
+import { CoursesResolver } from './services/courses.resolver';
 
 
 export const coursesRoutes: Routes = [
     {
         path: '',
-        component: HomeComponent
-
+        component: HomeComponent,
+        resolve: {
+            courses: CoursesResolver
+        }
     },
     {
         path: ':courseUrl',
-        component: CourseComponent
+        component: CourseComponent,
+        resolve: {
+            courses: CoursesResolver
+        }
     }
 ];
 
+const entityMetadata: EntityMetadataMap = {
+    Course: {
+        
+    }
+};
 
 @NgModule({
     imports: [
@@ -70,10 +84,19 @@ export const coursesRoutes: Routes = [
     ],
     entryComponents: [EditCourseDialogComponent],
     providers: [
-        CoursesHttpService
+        CoursesHttpService,
+        CourseEntityService,
+        CoursesResolver,
+        CoursesDataService
     ]
 })
 export class CoursesModule {
-    constructor() {
+    constructor(
+        private eds: EntityDefinitionService, 
+        private entityDataService: EntityDataService,
+        private coursesDataService: CoursesDataService,
+    ) {
+        eds.registerMetadataMap(entityMetadata);
+        entityDataService.registerService('Course', coursesDataService);
     }
 }
