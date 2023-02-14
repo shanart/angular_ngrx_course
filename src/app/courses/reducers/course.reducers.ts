307b7d213c9383ecb9c67a29266ac6c7da4b0003
shanart <shanin.x@gmail.com>
2023-02-14 02:30:12 +0200

    allCoursesLoaded state property and selector

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -3,18 +3,24 @@ import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CoursesActions } from "../action-types";
 
-export interface CoursesState extends EntityState<Course> {}
+
+export interface CoursesState extends EntityState<Course> {
+    allCoursesLoaded: boolean
+}
+
 export const adapter = createEntityAdapter<Course>({
     sortComparer: compareCourses
     // selectId: course => course.id  // here you can set what field is the unique identifier for entity
 });
-export const initialCoursesState = adapter.getInitialState();
+
+export const initialCoursesState = adapter.getInitialState({
+    allCoursesLoaded: false
+});
 
 export const coursesReducer = createReducer(
     initialCoursesState,
-    on(CoursesActions.allCoursesLoaded, (state, action) => adapter.setAll(action.courses, state))
+    on(CoursesActions.allCoursesLoaded, (state, action) => adapter.setAll(action.courses, {...state, allCoursesLoaded: true}))
 )
-
 
 export const {
     selectAll
