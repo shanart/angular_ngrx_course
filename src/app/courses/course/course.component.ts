237cd04cc5b0034af7d5f79a80cc1c05917cf1c2
shanart <shanin.x@gmail.com>
2023-02-14 23:08:36 +0200

    push detection

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,49 +1,56 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {Observable} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+import { Observable, of } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { concatMap, delay, filter, first, map, mapTo, shareReplay, tap, withLatestFrom } from 'rxjs/operators';
+import { CoursesHttpService } from '../services/courses-http.service';
+import { CourseEntityService } from '../services/course-entity.service';
+import { LessonEntityService } from '../services/lesson-entity.service';
 
 
 @Component({
-  selector: 'course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+    selector: 'course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
+    course$: Observable<Course>;
+    loading$: Observable<boolean>;
+    lessons$: Observable<Lesson[]>;
+    displayedColumns = ['seqNo', 'description', 'duration'];
+    nextPage = 0;
 
-  lessons$: Observable<Lesson[]>;
+    constructor(
+        private coursesService: CourseEntityService,
+        private lessonsService: LessonEntityService,
+        private route: ActivatedRoute) {
+    }
 
-  displayedColumns = ['seqNo', 'description', 'duration'];
+    ngOnInit() {
+        const courseUrl = this.route.snapshot.paramMap.get("courseUrl");
+        this.course$ = this.coursesService.entities$.pipe(
+            map(courses => courses.find(course => course.url === courseUrl))
+        );
+        this.lessons$ = this.lessonsService.entities$.pipe(
+            withLatestFrom(this.course$),
+            tap(([lessons, course]) => {
+                if (this.nextPage === 0) this.loadLessonsPage(course)
+            }),
+            map(([lessons, course]) => lessons.filter(lesson => lesson.courseId === course.id))
+        );
+        this.loading$ = this.lessonsService.loading$.pipe(delay(0));
+    }
 
-  nextPage = 0;
-
-  constructor(
-    private coursesService: CoursesHttpService,
-    private route: ActivatedRoute) {
-
-  }
-
-  ngOnInit() {
-
-    const courseUrl = this.route.snapshot.paramMap.get("courseUrl");
-
-    this.course$ = this.coursesService.findCourseByUrl(courseUrl);
-
-    this.lessons$ = this.course$.pipe(
-      concatMap(course => this.coursesService.findLessons(course.id)),
-      tap(console.log)
-    );
-
-  }
-
-
-  loadLessonsPage(course: Course) {
-
-  }
-
+    loadLessonsPage(course: Course) {
+        this.lessonsService.getWithQuery({
+            courseId: course.id.toString(),
+            pageNumber: this.nextPage.toString(),
+            pageSize: '3'
+        });
+        this.nextPage += 1;
+    }
 }
+
